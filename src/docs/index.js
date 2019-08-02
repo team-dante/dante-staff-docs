95c86258c13d49065bd465df0b2054834ecfb0c7
Xinhao Liang <xinhao128@hotmail.com>
2019-08-02 13:07:08 -0700

    Fixed packages

--- src/docs/index.js
+++ src/docs/index.js
@@ -9,8 +9,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './prism.css';
 import './index.css';
 
-const title = 'Component Template';
-const gh = 'reactstrap/component-template';
+const title = 'Dante Patient';
+const gh = 'team-dante/Dante-Staff-Swift';
 // if using a root url, remove the basename value here and in BrowserRouter
 const basename = process.env.REACT_APP_GH_PAGES_PATH ?
   `/${process.env.REACT_APP_GH_PAGES_PATH}` : '';
