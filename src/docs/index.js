728d93524f4a0844cb83da23bb4dc421a7d086fe
Xinhao Liang <xinhao128@hotmail.com>
2019-08-02 21:48:12 -0700

    Fixed Routing; should always scroll to top; updated favicon.ico

--- src/docs/index.js
+++ src/docs/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LayoutFooter from './layout/Footer';
 import Home from './Home';
 import Documentation from './Documentation';
+import ScrollToTop from './ScrollToTop';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './prism.css';
@@ -17,15 +18,17 @@ const basename = process.env.REACT_APP_GH_PAGES_PATH ?
 const Docs = () => {
   return (
     <BrowserRouter basename={basename}>
-      <div className="App">
-        {/* Begin Routes */}
-        <Switch>
-            <Route exact path="/" render={() => <Home title={title} gh={gh} />} />
-            <Route path="/documentation" component={Documentation} />
-        </Switch>
-        {/* End Routes */}
-        <LayoutFooter gh={gh} />
-      </div>
+      <ScrollToTop>
+        <div className="App">
+          {/* Begin Routes */}
+          <Switch>
+              <Route exact path="/" render={() => <Home title={title} gh={gh} />} />
+              <Route path="/documentation" component={Documentation} />
+          </Switch>
+          {/* End Routes */}
+          <LayoutFooter gh={gh} />
+        </div>
+      </ScrollToTop>
     </BrowserRouter>
   );
 };
